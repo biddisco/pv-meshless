32725ba7c37a5c056b2cfa7c7b74786477689e92
John Biddiscombe <biddisco@cscs.ch>
2012-05-19 23:43:19 +0200

    Revamp testing to make it easier to add new features
    Adding sc12 benchmark test Partition filter using BBoxes from reader.
    Lots of tweaks and tidy ups to cmake and tests
    Put testutils in a testlib for better linking
    use MPIExec even for N=1 tests
    Fix SC12 test setup for Todi testing
    Set Paths for faster filesystem on todi/rosa
    Improve partition test to use different CPU counts
    Add MPI depth flag to spread jobs across nodes if possible
    Add todi timing results
    Fix build error with no trilinos

---
+++ Testing/TestUtils.h
@@ -0,0 +1,195 @@
+//
+#include <iostream>
+#include <sstream>
+//
+// For VTK_USE_MPI 
+#include "vtkToolkits.h"     
+#ifdef VTK_USE_MPI
+  #include "vtkMPI.h"
+  #include "vtkMPIController.h"
+  #include "vtkMPICommunicator.h"
+#endif
+// Otherwise
+#include "vtkDummyController.h"
+//
+#include "Testing/Cxx/vtkTestUtilities.h"
+#include "Testing/Cxx/vtkRegressionTestImage.h"
+//
+#include "vtkSmartPointer.h"
+#include "vtkTimerLog.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+
+// CSCS
+#include "vtkStreamOutputWindow.h"
+#include "vtkH5PartReader.h"
+#include "vtkSPHManager.h"
+#include "vtkSPHProbeFilter.h"
+#include "vtkSPHImageResampler.h"
+// only defined if trilinos used
+class vtkParticlePartitionFilter;
+//----------------------------------------------------------------------------
+#if 0
+  #define OUTPUTTEXT(a) std::cout << (a);
+  #define testDebugMacro(a)  \
+  { \
+    vtkOStreamWrapper::EndlType endl; \
+    vtkOStreamWrapper::UseEndl(endl); \
+    vtkOStrStreamWrapper vtkmsg; \
+    vtkmsg << a << endl; \
+    OUTPUTTEXT(vtkmsg.str()); \
+    vtkmsg.rdbuf()->freeze(0); \
+  }
+#else
+  #define testDebugMacro(a) 
+#endif
+//----------------------------------------------------------------------------
+#define DEBUG_WAIT \
+  if (test.myRank==0) { \
+    char ch;       \
+    std::cout << "Attach debugger" << std::endl; \
+    std::cin >> ch; \
+  }
+//----------------------------------------------------------------------------
+class TestStruct {
+ public:
+  //
+  vtkSmartPointer<vtkMultiProcessController>  controller;
+  vtkSmartPointer<vtkH5PartReader>            reader;
+  vtkSmartPointer<vtkParticlePartitionFilter> partitioner;
+  vtkSmartPointer<vtkSPHManager>              sphManager;
+  vtkSmartPointer<vtkAlgorithm>               sphResampler;
+  //
+  vtkTypeInt64 myRank;
+  vtkTypeInt64 numProcs;
+  //--------------------------------------------------------------
+  // Testing params
+  //--------------------------------------------------------------
+  bool   unused, fixNeighbours, fixRadius, cameraSet;
+  double gridSpacing[3];
+  int    gridResolution[3];
+  double vminmax[2];
+  double vpos[3];
+  double cameraPosition[3];
+  double cameraFocus[3];
+  double cameraViewUp[3];
+  int    windowSize[2];
+  //
+  std::string testName;
+
+  bool        doRender;
+  bool        keepTempFiles;
+  //
+  // (Random) Particle Generation
+  //
+  vtkIdType   generateN;
+
+  //
+  // H5Part Reader 
+  //
+  bool        ReadData;
+  std::string fullName;
+  std::string Xarray;
+  std::string Yarray;
+  std::string Zarray;
+  bool        ignorePartitions;
+
+  //
+  // SPH kernel or neighbour info
+  //
+  double      particleSize;
+  double      ghostOverlap;
+  int         maxN;
+  std::string massScalars;
+  std::string densityScalars;
+  vtkIdType   expectedN;
+
+  //
+  // Test/Display of results
+  //
+  std::string scalarname;
+  double      contourVal;
+  bool        imageResample;
+  bool        skipImageTest;
+  std::string imageScalars;
+  int         imageThreshold;
+  bool        benchmarkPartition;
+  //
+  void    CreateReader();
+  double  UpdateReader();
+  void    CreatePartitioner();
+  double  UpdatePartitioner();
+  void    DeletePartitioner();
+  void    CreateSPHManager();
+  void    CreateSPHResampler(vtkAlgorithm *input);
+  double  UpdateSPHResampler();
+  //
+};
+//
+int initTest(int argc, char* argv[], TestStruct &test);
+//----------------------------------------------------------------------------
+unsigned long int random_seed();
+void known_seed();
+void SpherePoints(int n, float radius, float X[]);
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+template <typename T>
+void DisplayParameter(const char *prefix1, const char *prefix2, T *data, int components, int rank)
+{
+  if (rank==-1) {
+    return;
+  }
+  vtkstd::stringstream temp;
+  temp << prefix1 << prefix2 << std::ends;
+  std::cout.width(30);
+  std::cout << temp.str().c_str() << " : {";
+  std::cout.width(0);
+  for (int i=0; i<components; i++) {
+    std::cout << data[i];
+    (i==(components-1)) ? std::cout << "}" : std::cout << ",";
+  }
+  std::cout << std::endl;
+}
+//----------------------------------------------------------------------------
+template <typename T>
+T GetParameter(const char *argstr, const char *message, int argc, char **argv, T defaultvalue, int rank, bool &valueset)
+{
+  char *tempChar = vtkTestUtilities::GetArgOrEnvOrDefault(argstr, argc, argv, "", "");
+  T newValue = defaultvalue;
+  valueset = false;
+  if (std::string(tempChar).size()) {
+    vtkstd::stringstream temp(tempChar);
+    temp >> newValue;
+    if (rank==0) {
+      DisplayParameter<T>(message, "", &newValue, 1, rank);
+    }
+    valueset = true;
+  }
+  delete []tempChar;
+  return newValue;
+}
+//----------------------------------------------------------------------------
+template <typename T>
+bool GetArrayParameter(const char *argstr, const char *message, T *data, int components, int argc, char **argv, int rank)
+{
+  char *tempChar = vtkTestUtilities::GetArgOrEnvOrDefault(argstr, argc, argv, "", "");
+  bool valueset = false;
+  if (std::string(tempChar).size()) {
+    vtkstd::stringstream temp(tempChar);
+    for (int i=0; i<components; i++) temp >> data[i];
+    if (rank==0) {
+      std::cout.width(30);
+      std::cout << message << " : {";
+      std::cout.width(0);
+      for (int i=0; i<components; i++) {
+        std::cout << data[i];
+        (i==(components-1)) ? std::cout << "}" : std::cout << ",";
+      }
+      std::cout << std::endl;
+    }
+    valueset = true;
+  }
+  delete []tempChar;
+  return valueset;
+}
+//----------------------------------------------------------------------------
