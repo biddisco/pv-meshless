4c1f4c008924e68b4841ec42d50015e9b62a9db5
John Biddiscombe <biddisco@cscs.ch>
2012-08-28 15:43:13 +0200

    Scripts for generating changelog

---
+++ scripts/changelog.sh
@@ -0,0 +1,1 @@
+git for-each-ref --sort=-authordate 'refs/tags/SPHERIC*' 'refs/heads/release*' | awk '{printf $3 " "}' | sed 's/refs\/tags\///g' | sed 's/refs\/heads\///g' |  awk '{ for (i=1;i<NF; i++) print $(i+1) " " $i; print $NF}' | xargs -n 2 ./scripts/prettylog.sh
---
+++ scripts/prettylog.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# $1 from tag
+# $2 to tag
+
+# 05729f831f   SPHERIC-2011
+# SPHERIC-2011 release-3.10
+# release-3.10 SPHERIC-2012
+# SPHERIC-2012 release-3.14
+
+# echo "SPHERIC-2011" > tmp.txt && cat tmp.txt ChangeLog.txt > tmp2.txt && mv tmp2.txt ChangeLog.txt
+
+if [ -n "$2" ]
+ then 
+  echo -e "\n"$2"\n" 
+  git log $1..$2 --no-merges --pretty=format:"%ad %x09 %an%x09 %h %s" --date=short | gawk '{printf("%s %-24s %s ", $1, $2" "$3, $4); $1=$2=$3=$4=""; print $0}'
+else
+  echo -e "\n"$1"\n"
+  git log $1 --no-merges --pretty=format:"%ad %x09 %an%x09 %h %s" --date=short | gawk '{printf("%s %-24s %s ", $1, $2" "$3, $4); $1=$2=$3=$4=""; print $0}'
+fi
